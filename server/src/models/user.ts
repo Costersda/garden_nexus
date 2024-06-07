ac0c7284bf74afb621d69b0d0152761a76e596e9
Costersda <costersda@gmail.com>
2024-06-07 20:13:05 +1200

    created user interface and model

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,6 +1,7 @@
 import { Schema, model } from 'mongoose';
 import { UserDocument } from '../types/user.interface';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new Schema<UserDocument>({
     email: { 
@@ -22,5 +23,24 @@ const userSchema = new Schema<UserDocument>({
 }, {
     timestamps: true,
 });
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        return next();
+    } catch (err) {
+        return next(err as Error)
+    }
+
+});
+
+userSchema.methods.validatePassword = function(password: string) {
+    return bcrypt.compare(password, this.password);
+};
 
 export default model<UserDocument>('User', userSchema);
