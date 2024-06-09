3429a4b86c223aef70ee8d7ddb98aee5617ba61a
Costersda <costersda@gmail.com>
2024-06-10 11:49:45 +1200

    added login/ register functionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (currentUser) => {
+        this.authService.setCurrentUser(currentUser);
+      },
+      error: (err) => {
+        console.log('err', err);
+        this.authService.setCurrentUser(null);
+      },
+    });
+
+  }
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,36 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
-import mongoose from 'mongoose';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import * as usersController from "./controllers/users";
+import bodyParser from "body-parser";
+import authMiddleware from "./middlewares/auth";
+import cors from "cors";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('API is up');
+app.get("/", (req, res) => {
+  res.send("API is UP");
 });
 
-io.on('connection', () => {
-    console.log('connect');
+app.post("/api/users", usersController.register);
+app.post("/api/users/login", usersController.login);
+app.get('/api/user', authMiddleware, usersController.currentUser)
+
+io.on("connection", () => {
+  console.log("connect");
 });
 
-mongoose.connect('mongodb://localhost:27017/garden_nexus').then(() => {
-    console.log('Connected to MongoDB');
-
-    httpServer.listen(4001, () => {
-        console.log('Server is running on port 4001');
-      });
+mongoose.connect("mongodb://localhost:27017/garden_nexus").then(() => {
+  console.log("connected to mongodb");
+  httpServer.listen(4001, () => {
+    console.log(`API is listening on port 4001`);
+  });
 });
-
