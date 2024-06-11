5f5a755845d9b9c7bdef021ed4e3ded582a49f77
Costersda <costersda@gmail.com>
2024-06-11 19:10:42 +1200

    completed the shared footer

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ToolbarComponent } from '../shared/toolbar/toolbar.component';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 
 
@@ -15,7 +16,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-    imports: [CommonModule, RouterModule.forChild(routes), ToolbarComponent, NavbarComponent],
+    imports: [CommonModule, RouterModule.forChild(routes), ToolbarComponent, NavbarComponent, FooterComponent],
     declarations: [HomeComponent],
 })
 
