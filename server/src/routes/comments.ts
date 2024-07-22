ffc40ced7c080cdd95744392f53b302c71969cf1
Costersda <costersda@gmail.com>
2024-07-22 17:52:11 +1200

    commented out routes folder

--- server/src/routes/comments.ts
+++ server/src/routes/comments.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import * as commentsController from '../controllers/comments';
-import authMiddleware from '../middlewares/auth';
+// import express from 'express';
+// import * as commentsController from '../controllers/comments';
+// import authMiddleware from '../middlewares/auth';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/', authMiddleware, commentsController.createComment);
-router.get('/blog/:blogId', commentsController.getAllCommentsByBlogId);
-router.get('/forum/:forumId', commentsController.getAllCommentsByForumId);
-router.get('/:id', commentsController.getCommentById);
-router.patch('/:id', authMiddleware, commentsController.updateCommentById);
-router.delete('/:id', commentsController.deleteCommentById);
+// router.post('/', authMiddleware, commentsController.createComment);
+// router.get('/blog/:blogId', commentsController.getAllCommentsByBlogId);
+// router.get('/forum/:forumId', commentsController.getAllCommentsByForumId);
+// router.get('/:id', commentsController.getCommentById);
+// router.patch('/:id', authMiddleware, commentsController.updateCommentById);
+// router.delete('/:id', commentsController.deleteCommentById);
 
-export default router;
+// export default router;
--- server/src/routes/forums.ts
+++ server/src/routes/forums.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import * as forumController from '../controllers/forums';
-import authMiddleware from '../middlewares/auth';
+// import express from 'express';
+// import * as forumController from '../controllers/forums';
+// import authMiddleware from '../middlewares/auth';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/', authMiddleware, forumController.createForum);
-router.get('/', forumController.getAllForums);
-router.get('/search', forumController.getForumsBySearch);
-router.get('/category', forumController.getForumsByCategory);
-router.get('/user/:username', forumController.getForumsByUser);
-router.get('/:id', forumController.getForumWithUserById);
-router.patch('/:id', authMiddleware, forumController.updateForumById);
-router.delete('/:id', forumController.deleteForumById);
+// router.post('/', authMiddleware, forumController.createForum);
+// router.get('/', forumController.getAllForums);
+// router.get('/search', forumController.getForumsBySearch);
+// router.get('/category', forumController.getForumsByCategory);
+// router.get('/user/:username', forumController.getForumsByUser);
+// router.get('/:id', forumController.getForumWithUserById);
+// router.patch('/:id', authMiddleware, forumController.updateForumById);
+// router.delete('/:id', forumController.deleteForumById);
 
-export default router;
+// export default router;
